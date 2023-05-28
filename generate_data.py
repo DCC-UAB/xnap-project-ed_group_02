f184fdebe941df3d6087e0ca4a887c1eab93687f
danielpaulisch <81854274+danielpaulisch@users.noreply.github.com>
2023-05-28 22:08:43 +0200

    cambiat generador de dades

--- generate_data.py
+++ generate_data.py
@@ -15,6 +15,7 @@ import io
 import re
 
 dir_trainfolder = "./gtzan/_train"
+destination = "gtzan_complete/_train/"
 
 list_of_audiofiles=path_to_audiofiles(dir_trainfolder)
 
@@ -27,8 +28,14 @@ for i, file in enumerate(list_of_audiofiles):
     RMS=math.sqrt(np.mean(data**2))
     noise=np.random.normal(0, RMS, data.shape[0])
     signal_noise = data+ noise*0.4
+    if i%2==0:
+        signal_volume =data *0.5
+    else:
+        signal_volume =data *2
+
     splits = re.split("[ .]", file)
     genre = re.split("[ /]", splits[1])[3]
 
-    sf.write("gtzan_noise/"+genre+".001"+splits[2][3:]+".au",signal_noise,samplerate)
+    sf.write(destination+genre+".001"+splits[2][3:]+".au",signal_noise,samplerate)
+    sf.write(destination+genre+".002"+splits[2][3:]+".au",signal_volume,samplerate)
 
