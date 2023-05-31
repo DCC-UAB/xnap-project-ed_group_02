fd22b01bf1920dbd70359e464f9dddfa3b443778
danielpaulisch <81854274+danielpaulisch@users.noreply.github.com>
2023-05-31 20:13:36 +0200

    arreglat entrenament

--- entrenament.py
+++ entrenament.py
@@ -9,6 +9,7 @@ import seaborn as sns
 
 def entrenar(model,conjunt_entrenament,optimizer,scheduler=None,loss_function=nn.NLLLoss(),num_epochs=400,batch_size=42,grafiquejar=True):
     train_X,train_Y,dev_X,dev_Y,test_X,test_Y=conjunt_entrenament
+    
     train_on_gpu = torch.cuda.is_available()
     if train_on_gpu:
         print("\nTraining on GPU")
@@ -23,9 +24,8 @@ def entrenar(model,conjunt_entrenament,optimizer,scheduler=None,loss_function=nn
     num_dev_batches = int(dev_X.shape[0] / batch_size)
 
     val_loss_list, val_accuracy_list, epoch_list = [], [], []
-    train_accuracy_list=[]
-    train_loss_list=[]
-    epoch_train_list=[]
+    train_accuracy_list,train_loss_list,epoch_train_list=[],[],[]
+
     print("Training ...")
     for epoch in range(num_epochs):
 
@@ -98,8 +98,8 @@ def entrenar(model,conjunt_entrenament,optimizer,scheduler=None,loss_function=nn
                 
                 for i in range(num_dev_batches):
                     X_local_validation_minibatch, y_local_validation_minibatch = (
-                        dev_X[i * batch_size: (i + 1) * batch_size, ],
-                        dev_Y[i * batch_size: (i + 1) * batch_size, ],
+                        dev_X[:],
+                        dev_Y[:],
                     )
                     X_local_minibatch = X_local_validation_minibatch.permute(1, 0, 2)
                     y_local_minibatch = torch.max(y_local_validation_minibatch, 1)[1]
